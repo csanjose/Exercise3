d2153b8e47024261e592ce72ca453a4d80517384
csanjose <csanjoser@gmail.com>
2015-08-24 00:02:55 +0200

    Initial commit

---
+++ best.R
@@ -0,0 +1,22 @@
+best <- function(state, outcome) {
+  
+  if (outcome=="heart attack") columna=11
+  if (outcome=="heart failure") columna=17
+  if (outcome=="pneumonia") columna=23
+  
+  if(!any(state == data$State)) stop('invalid state')
+  
+  datos <- read.csv("outcome-of-care-measures.csv")
+  
+  datos2 <- subset(datos, datos$State==state)
+  
+  datos3 <- datos2[c(2,7,columna)]
+  
+  datos4 <- datos3[which(datos3[3] != "Not Available"), ]
+  
+  row <- which.min(as.numeric(as.character(unlist(datos4[3]))))
+  
+  as.character(datos4[row, "Hospital.Name"])
+  
+  
+}
